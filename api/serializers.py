4f5c1427bd9101376e65af6e77a4817d39f62fcb
Baba905 <baba78122@gmail.com>
2025-07-30 23:32:43 +0000

    Nested serializers, SerializerMethodField, Serializer Relations

--- api/serializers.py
+++ api/serializers.py
@@ -19,3 +19,38 @@ class ProductSerializer(serializers.ModelSerializer):
         if price <= 0:
             raise serializers.ValidationError("Price can't be a negative value")
         return price 
+    
+class OrderItemSerializer(serializers.ModelSerializer):
+    # We get all attributs of Product class
+    ## product = ProductSerializer()
+    # Use the below approch if you some attributs not all
+    product_name = serializers.CharField(source='product.name')
+    product_price = serializers.DecimalField(max_digits=10,decimal_places=2,source='product.price')
+    class Meta:
+        model = OrderItem
+        fields =[
+            "product_name",
+            "product_price",
+            "quantity",
+            "item_subtotal",
+        ]
+    
+class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many = True, read_only= True)
+    total_price = serializers.SerializerMethodField()
+
+    def get_total_price(self, obj):
+        order_items = obj.items.all()
+        return sum(order_item.item_subtotal for order_item in order_items)
+    
+    class Meta:
+        model = Order
+        fields = [
+            "order_id",
+            "user",
+            "created_at",
+            "status",
+            "items",
+            "total_price"
+        ]
+
