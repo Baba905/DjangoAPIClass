d3f67e2c977febc2892d4650d004c27da2d0fc8a
Baba905 <baba78122@gmail.com>
2025-08-05 19:08:50 +0000

    Add django silk to check performance. Added also the prefetch_related to optimise the queryset performance of the API

--- APICore/urls.py
+++ APICore/urls.py
@@ -20,4 +20,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("api.urls")),
+    path('silk/', include('silk.urls', namespace='silk')),
 ]
+
