c665889bbeb538862275eab3fd91c9cc5444f330
Baba905 <baba78122@gmail.com>
2025-08-07 15:55:45 +0000

    Use the class base view with genericAPIview

--- api/urls.py
+++ api/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('products/', views.product_list),
+    path('products/', views.ProductList.as_view()),
     path('products/info/', views.product_info),
-    path('products/<int:pk>/', views.product_details),
-    path('orders/', views.order_list),
+    path('products/<int:product_id>/', views.ProductDetail.as_view()),
+    path('orders/', views.OrderList.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -4,26 +4,30 @@ from api.serializers import ProductSerializer, OrderSerializer,ProductInfoSerial
 from api.models import Product, Order, OrderItem, User
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import generics
 
-@api_view(['GET'])
-def product_list(request):
-    products= Product.objects.all()
-    serializer = ProductSerializer(products,many=True)
-    return Response(serializer.data)
+""""Function base view """
 
-
-@api_view(['GET'])
-def product_details(request,pk):
-    product= get_object_or_404(Product,pk=pk)
-    serializer = ProductSerializer(product)
-    return Response(serializer.data)
+## Function base view for product class
+# @api_view(['GET'])
+# def product_list(request):
+#     products= Product.objects.all()
+#     serializer = ProductSerializer(products,many=True)
+#     return Response(serializer.data)
 
 
-@api_view(['GET'])
-def order_list(request):
-    orders= Order.objects.prefetch_related('items', 'items__product')
-    serializer = OrderSerializer(orders,many=True)
-    return Response(serializer.data)
+# @api_view(['GET'])
+# def product_details(request,pk):
+#     product= get_object_or_404(Product,pk=pk)
+#     serializer = ProductSerializer(product)
+#     return Response(serializer.data)
+
+
+# @api_view(['GET'])
+# def order_list(request):
+#     orders= Order.objects.prefetch_related('items', 'items__product')
+#     serializer = OrderSerializer(orders,many=True)
+#     return Response(serializer.data)
 
 
 @api_view(['GET'])
@@ -37,4 +41,23 @@ def product_info(request):
     print(products.aggregate(max_price=Max('price')))
     return Response(serializer.data)
 
+
+
+""""Class base view"""
+# https://www.django-rest-framework.org/api-guide/generic-views/#generic-views
+# Class base view for product
+
+class ProductList(generics.ListAPIView):
+    #queryset = Product.objects.all()
+    queryset = Product.objects.filter(stock__gt=0)
+    serializer_class = ProductSerializer
+
+class ProductDetail(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_url_kwarg = 'product_id'
+
+class OrderList(generics.ListAPIView):
+    queryset= Order.objects.prefetch_related('items', 'items__product')
+    serializer_class = OrderSerializer
 
